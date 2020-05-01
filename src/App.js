8005ddb8bc3a5dc27a7986a6312bbc9ce71655aa
Reet Priye <reetpriye@gmail.com>
2020-05-01 20:33:18 +0530

    initial test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./styles.css";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Testing</h1>
     </div>
   );
 }
+
+export default App;
