bf7eab5b58ca676e6b366f48d753389ede497de5
Reet Priye <reetpriye@gmail.com>
2020-05-02 19:32:47 +0530

    countryselector functional

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import coronaImage from "./images/image.png";
 
 class App extends React.Component {
   state = {
@@ -22,12 +23,13 @@ class App extends React.Component {
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
