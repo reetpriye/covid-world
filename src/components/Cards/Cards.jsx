9a8787a16455278614e8b3d0a807ad7981df3460
Reet Priye <reetpriye@gmail.com>
2020-05-02 12:57:39 +0530

    implement countup

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
+import CountUp from "react-countup";
 
 const Cards = ({ data: { TotalConfirmed, TotalDeaths, TotalRecovered } }) => {
   if (!TotalConfirmed) {
@@ -14,7 +15,14 @@ const Cards = ({ data: { TotalConfirmed, TotalDeaths, TotalRecovered } }) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">{TotalConfirmed}</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={TotalConfirmed}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATA</Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
@@ -26,7 +34,14 @@ const Cards = ({ data: { TotalConfirmed, TotalDeaths, TotalRecovered } }) => {
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">{TotalRecovered}</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={TotalRecovered}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATA</Typography>
             <Typography variant="body2">
               Number of recoveries from COVID-19
@@ -38,7 +53,14 @@ const Cards = ({ data: { TotalConfirmed, TotalDeaths, TotalRecovered } }) => {
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">{TotalDeaths}</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={TotalDeaths}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATA</Typography>
             <Typography variant="body2">
               Number of Deaths caused by COVID-19
