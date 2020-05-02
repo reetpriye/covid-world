ef0cdeb12cfc3d7b4effe22bb42633c814b57ada
Reet Priye <reetpriye@gmail.com>
2020-05-02 11:39:05 +0530

    api data fetch display

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 
-const Cards = props => {
-  console.log(props);
-
+const Cards = ({ data: { TotalConfirmed, TotalDeaths, TotalRecovered } }) => {
+  if (!TotalConfirmed) {
+    return "Loading...";
+  }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -13,10 +14,34 @@ const Cards = props => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">{TotalConfirmed}</Typography>
             <Typography color="textSecondary">REAL DATA</Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">{TotalRecovered}</Typography>
+            <Typography color="textSecondary">REAL DATA</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">{TotalDeaths}</Typography>
+            <Typography color="textSecondary">REAL DATA</Typography>
+            <Typography variant="body2">
+              Number of Deaths caused by COVID-19
             </Typography>
           </CardContent>
         </Grid>
